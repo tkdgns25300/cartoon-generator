47f81bc01f17002d7aa9663315132822bd190546
이상훈 <tkdgns25300@gmail.com>
2025-06-18 20:36:43 +0900

    initial commit

---
+++ src/main/java/com/sanghun/cartoon_generator/CartoonGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.sanghun.cartoon_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartoonGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartoonGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanghun/cartoon_generator/CartoonGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanghun.cartoon_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartoonGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
